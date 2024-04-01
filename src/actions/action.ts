595b753e3371d5fd98359034fc9d7ab3fbc11673
hianshul07 <hianshul07@gmail.com>
2024-04-01 23:41:26 +0530

    updates

---
+++ src/actions/action.ts
@@ -0,0 +1,10 @@
+'use server'
+
+import db from "@/db"
+
+const editSnippet = async (id: number, code: string) => {
+  console.log(id, code);
+  
+}
+
+export default editSnippet
--- src/components/SnipptEditForm.tsx
+++ src/components/SnipptEditForm.tsx
@@ -1,13 +1,38 @@
-'use client'
-import type { Snippet } from "@prisma/client"
+'use client';
+
+import type { Snippet } from '@prisma/client';
+import { Editor } from '@monaco-editor/react';
+import { useState } from 'react';
+import editSnippet from '@/actions/action';
 
 interface Props {
-  snippet: Snippet
+	snippet: Snippet;
 }
 
-const SnipptEditForm = ({snippet}: Props) => {
-  return (
-    <div>SnipptEditForm</div>
-  )
-}
-export default SnipptEditForm
+const SnipptEditForm = ({ snippet }: Props) => {
+	const [code, setCode] = useState(snippet.code);
+	const handleEditorChange = (value: string = '') => {
+		setCode(value);
+	};
+
+	const snippetEditAction = editSnippet.bind(null, snippet.id, code);
+
+	return (
+		<div>
+			<Editor
+				height='40vh'
+				theme='vs-dark'
+				defaultLanguage='typescript'
+				defaultValue={snippet.code}
+				options={{ minimap: { enabled: false } }}
+				onChange={handleEditorChange}
+			/>
+			<form action={snippetEditAction}>
+				<button type='submit' className='p-2 border rounded'>
+					Edit
+				</button>
+			</form>
+		</div>
+	);
+};
+export default SnipptEditForm;
